346b60c696c02e73a6ca545cd1229ca3dfac186e
Brian Li <Brian.Li96@yahoo.com>
2021-01-10 10:25:18 -0600

    commit tree bd8bd7a9df3f1717a8a9c7e18f5d3e6679cb5e45

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(arr, string)
+{
+    let newArr = [];
+    for(let i = 0; i <arr.length; i++)
+    {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(num)
+{
+    while(num > -1)
+    {
+        console.log(num);
+        num--;
+    }
+}
